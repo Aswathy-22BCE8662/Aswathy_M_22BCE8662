c43f55128365358675c256284afff737644d1a99
Aswathy M <aswathy.22bce8662@vitapstudent.ac.in>
2025-06-17 16:44:37 +0530

    Delete Final Project/Source Code/LIBRARY_SYSTEM directory

--- Final Project/Source Code/LIBRARY_SYSTEM/extensions.py
+++
@@ -1,8 +0,0 @@
-# extensions.py
-from flask_pymongo import PyMongo
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-
-mongo = PyMongo()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
